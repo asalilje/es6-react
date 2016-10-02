9f3d11c4b4dd84a4129565822ab2bc3666657f2e
Åsa Liljegren <asa.lilje@gmail.com>
2016-10-02 22:27:38 +0200

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,31 @@
+const UrlConfig = require('UrlConfig');
+import React, {PropTypes} from 'react';
+import {connect} from 'react-redux';
+import Header from './common/Header';
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="container-fluid">
+        {UrlConfig.greeting}
+        <Header loading={this.props.loading}/>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.object.isRequired,
+  loading: PropTypes.bool.isRequired
+};
+
+function mapStateToProps(state, ownProps) {
+  return {
+    loading: state.numAjaxCallsInProgress > 0
+  };
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/components/common/TextInput.js
@@ -0,0 +1,34 @@
+import React, {PropTypes} from 'react';
+
+const TextInput = ({name, label, onChange, placeholder, value, error}) => {
+  let wrapperClass = 'form-group';
+  if (error && error.length > 0) {
+    wrapperClass += ' has-error';
+  }
+
+  return (
+    <div className={wrapperClass}>
+      <label htmlFor={name}>{label}</label>
+      <input
+        type="text"
+        className="form-control"
+        name={name}
+        value={value}
+        placeholder={placeholder}
+        onChange={onChange}/>
+      {error && <div className="alert alert-danger">{error}</div>}
+    </div>
+  );
+};
+
+TextInput.propTypes = {
+  name: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string,
+  error: PropTypes.string
+};
+
+export default TextInput;
+
